f72be056f4ae1e115d3049d122e4483705bf1ea8
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-24 17:32:43 +0000

    setting up redis, adding promisify to it

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,6 +14,20 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    //to avoid using callbacks in client.get we promisify client.get
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    // do we have any cached date in redis related to this query?
+    const cachedBlogs = await client.get(req.user.id);
+
+    // if yes, then respond to the request right away and return
+
+    // if no, we need to respond to request and update our cache to store the data
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
