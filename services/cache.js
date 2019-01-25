656c26ccec08319b4626b9cacaff10dce1ad748e
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-25 11:18:09 +0000

    refactoring redis logic to store hash values

--- services/cache.js
+++ services/cache.js
@@ -5,14 +5,15 @@ const util = require('util');
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 //to avoid using callbacks in client.get we promisify client.get
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 // store the reference of the original exec function
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   // set a flag to cahce the results
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || 'default');
 
   // make this function chainable
   return this;
@@ -28,7 +29,7 @@ mongoose.Query.prototype.exec = async function() {
   const key = JSON.stringify({...this.getQuery(), collection: this.mongooseCollection.name});
 
   // see if we have any a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // if we do, return that
   if (cacheValue) {
@@ -49,7 +50,7 @@ mongoose.Query.prototype.exec = async function() {
 
   // setting cache auto expiry for 10 secondes
   // note: keys that already exist in redis wont have this expiry time
-  client.set(key, JSON.stringify(result), 'EX', 10);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
   return result;
 
