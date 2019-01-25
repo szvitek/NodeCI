49ccc0b74ac6c00cd97e13f71b5d5c2bbff71487
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-25 10:46:21 +0000

    working caching setup

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,42 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+//to avoid using callbacks in client.get we promisify client.get
+client.get = util.promisify(client.get);
 
 // store the reference of the original exec function
 const exec = mongoose.Query.prototype.exec;
 
 // must use function() {} bc of 'this'
-mongoose.Query.prototype.exec = function() {
-  console.log('run a query');
+mongoose.Query.prototype.exec = async function() {
+  // do not modify the result of getQuery because it may modify the results, so prepare a new object for the cahce key
+  const key = JSON.stringify({...this.getQuery(), collection: this.mongooseCollection.name});
 
-  return exec.apply(this, arguments);
+  // see if we have any a value for 'key' in redis
+  const cacheValue = await client.get(key);
+
+  // if we do, return that
+  if (cacheValue) {
+    // cacheValue is a JSON but the app expexts a Mongoose Model, so we need to hydrate model
+    // but the problem is the new model is an array
+    //const doc = new this.model(JSON.parse(cacheValue));
+
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // otherwise, issue the query and store the result in redis
+  // exec returns a mongoose document
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
+
 }
