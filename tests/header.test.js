f1fc36aaffcd44561b2904d5546a2fd6fe432062
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-29 14:18:32 +0000

    launching chromium, navigate page, extracting content before/after each, auto-closing chromium

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,20 @@
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
+const puppeteer = require('puppeteer');
 
-  expect(sum).toEqual(3);
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('we can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
