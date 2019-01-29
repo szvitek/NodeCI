538193c2680a1857f2d9eba30f1c6f91c46e49c5
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-29 18:01:48 +0000

    test start oauth flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,7 +14,15 @@ afterEach(async () => {
   await browser.close();
 });
 
-test('we can launch a browser', async () => {
+test('the header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
