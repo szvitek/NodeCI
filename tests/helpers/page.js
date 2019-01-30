7ee98e1ff61d3724fcfee3306845590e7936ead7
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-30 12:17:06 +0000

    fixing close method in proxy

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -7,7 +7,7 @@ class CustomPage {
     });
 
     const page = await browser.newPage();
-    const customPage = new CustomPage(page);
+    const customPage = new CustomPage(page, browser);
 
     return new Proxy(customPage, {
       get: function(target, property) {
@@ -16,8 +16,15 @@ class CustomPage {
     })
   }
 
-  constructor(page) {
+  constructor(page, browser) {
     this.page = page;
+    this.browser = browser;
+  }
+
+  // browser.close() not being calld because page has a close method too which has a higher priority in the proxy function
+  // insted of this method we could have changed the order of page <=> browser
+  close() {
+    this.browser.close();
   }
 }
 
