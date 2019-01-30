a95866a60da7e65c9d6c2f775e84c01f5a30c868
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-30 10:04:52 +0000

    faking session in test, testing login

--- tests/header.test.js
+++ tests/header.test.js
@@ -26,3 +26,30 @@ test('clicking login starts oauth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test('when signed in, shows logout button', async () => {
+  // generating fake session object
+  const id = '5c48abe23443305790f7862e';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session='+sessionString);
+
+  // set the cookie
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');    // need to reload the page to "activate" the cookies
+  await page.waitFor('a[href="/auth/logout"]');   // need to tell puppeteer to wait for the given element appears on screen
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
+});
