407ea5cbbe7c9ced5057c1ad8e020fa806253177
Gyula Szvitek <gyula.szvitek@sqsltd.co.uk>
2019-01-31 16:32:29 +0000

    nested describes for structure

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,10 +11,17 @@ afterEach(async () => {
   await page.close();
 });
 
-test('when logged in, can see blog create form', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
-  const label = await page.getContentsOf('form label');
 
-  expect(label).toEqual('Blog Title');
+describe('When logged in', async () => {
+  beforeEach(async() => {
+    await page.login();
+    await page.click('a.btn-floating')
+  });
+
+  test('can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+  });
+
 });
